8e08ceb8bd37bb504551cc2e425f3230ed760deb
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-09-23 14:58:52 -0400

    create view profile details functionality

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -14,6 +14,23 @@ function index(req, res) {
   })
 }
 
+function show(req, res) {
+  Profile.findById(req.params.profileId)
+  .then(profile => {
+    const isSelf = profile._id.equals(req.user.profile._id)
+    res.render("profiles/show", {
+      title: `🗻 ${profile.name}'s profile`,
+      profile,
+      isSelf,
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+    res.redirect("/profiles")
+  })
+}
+
 export {
-  index
+  index,
+  show
 }
