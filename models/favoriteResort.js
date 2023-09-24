40208287f20e2352ca5bc98eb89f6244d53e3a98
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-09-24 09:37:30 -0400

    create FavoriteResort Model

---
+++ models/favoriteResort.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const favoriteResortSchema = new Schema({
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"},
+  resort: {type: Schema.Types.ObjectId, ref: "Resort"}
+}, {
+  timestamps: true
+})
+
+const FavoriteResort = mongoose.model('FavoriteResort', favoriteResortSchema)
+
+export {
+  FavoriteResort 
+}
