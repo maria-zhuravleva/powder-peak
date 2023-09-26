8d59a4b90b3405bde56e69b47406911bb55b0bb6
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-09-26 14:15:27 -0400

    create customized middleware to check if user is authorized

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,3 +1,6 @@
+import { Resort } from '../models/resort.js'
+import { User } from '../models/user.js'
+
 function passDataToView(req, res, next) {
   res.locals.user = req.user ? req.user : null
   res.locals.googleClientID = process.env.GOOGLE_CLIENT_ID
@@ -9,7 +12,25 @@ function isLoggedIn(req, res, next) {
   res.redirect('/')
 }
 
+
+function isAuthorized(req, res, next) {
+  const loggedUserId = req.user.id
+  const authorizedUser = '650e2b53d428f18bfd988a96'
+
+  // console.log('Logged User ID:', loggedUserId)
+  // console.log('Authorized ID:', authorizedUser)
+
+    if (loggedUserId === authorizedUser) {
+      return next()
+    } else {
+      res.redirect('/')
+    }
+}
+
+
+
 export {
   passDataToView,
   isLoggedIn,
+  isAuthorized
 }
--- routes/resorts.js
+++ routes/resorts.js
@@ -1,20 +1,21 @@
 import { Router } from 'express'
 import * as resortsCtrl from '../controllers/resorts.js'
 import { isLoggedIn } from '../middleware/middleware.js'
+import { isAuthorized } from '../middleware/middleware.js'
 
 const router = Router()
 
 router.get('/', resortsCtrl.index)
-router.get('/new', isLoggedIn, resortsCtrl.new)
+router.get('/new', isAuthorized, resortsCtrl.new)
 router.get('/:resortId', isLoggedIn, resortsCtrl.show)
-router.get("/:resortId/edit", isLoggedIn, resortsCtrl.edit)
+router.get("/:resortId/edit", isAuthorized, resortsCtrl.edit)
 router.get('/:resortId/reviews/:reviewId/edit', isLoggedIn, resortsCtrl.editReview)
-router.post('/', isLoggedIn, resortsCtrl.create)
+router.post('/', isAuthorized, resortsCtrl.create)
 router.post('/:resortId/reviews', isLoggedIn, resortsCtrl.createReview)
 router.post('/:resortId/favoriteResorts', isLoggedIn, resortsCtrl.addFavoriteResort)
-router.put("/:resortId", isLoggedIn, resortsCtrl.update)
+router.put("/:resortId", isAuthorized, resortsCtrl.update)
 router.put('/:resortId/reviews/:reviewId', isLoggedIn, resortsCtrl.updateReview)
-router.delete("/:resortId", isLoggedIn, resortsCtrl.delete)
+router.delete("/:resortId", isAuthorized, resortsCtrl.delete)
 router.delete('/:resortId/reviews/:reviewId', isLoggedIn, resortsCtrl.deleteReview)
 router.delete('/:resortId/favoriteResorts/:favoriteResortId', isLoggedIn, resortsCtrl.deleteFavoriteResort)
 
